c305a4a3c5691d5611e3b18649985df76b6ab939
Florian Krause <siebenhundertzehn@gmail.com>
2022-07-15 13:50:53 +0200

    Update _finish_the_job.py

--- finish_the_job/_finish_the_job.py
+++ finish_the_job/_finish_the_job.py
@@ -205,10 +205,12 @@ def get_masks(bold_files):
 
     """
 
+    import os
     import re
 
     masks = [x.replace("preproc_bold", "brain_mask") for x in bold_files]
-    return [re.sub("echo-[0-9]_", "", x) for x in masks]  # new since fmriprep 21.0.0
+    return  [re.sub("echo-[0-9]_", "", x) if not os.path.isfile(x) \
+             else x for x in masks]  # new since fmriprep 21.0.0
 
 def get_output_filename(bold_filename, suffix):
     """Get output filename for given bold filename and suffix.
