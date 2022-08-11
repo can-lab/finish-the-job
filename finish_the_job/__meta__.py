f946c0ad040f3948ffd9c693a545db924095c60c
Florian Krause <siebenhundertzehn@gmail.com>
2022-08-11 17:18:48 +0200

    fix for get_masks

--- finish_the_job/__meta__.py
+++ finish_the_job/__meta__.py
@@ -1,4 +1,4 @@
 __author__ = "Florian Krause <f.krause@donders.ru.nl>, \
               Martin Krentz <m.krentz@donders.ru.nl>"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __date__ = "2022-08-11"
--- finish_the_job/_finish_the_job.py
+++ finish_the_job/_finish_the_job.py
@@ -210,9 +210,9 @@ def get_masks(bold_files):
     import os
     import re
 
-    #masks = [x.replace("preproc_bold", "brain_mask") for x in bold_files]
-    masks = [re.sub("_desc-.+_bold", "_desc-brain_mask", x) for x in \
-             bold_files]
+    masks = [re.sub("desc-.+_bold", "desc-brain_mask", x) for x in  bold_files]
+    masks = [x.replace("space-MNI152NLin2009cAsym", "space-MNI152NLin6Asym") \
+             if not os.path.isfile(x) else x for x in masks]
     return  [re.sub("echo-[0-9]_", "", x) if not os.path.isfile(x) \
              else x for x in masks]  # new since fmriprep 21.0.0
 
